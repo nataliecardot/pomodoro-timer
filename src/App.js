e887df170bb9874e339565bb2378fcd3d6fad2a7
Natalie-Cardot <nataliecardot@gmail.com>
2019-07-12 23:35:49 -0700

    Establish basic layout and remove unneeded stuff from Create React App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Timer from './Timer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      sessionDuration: 1500,
+      breakDuration: 300,
+      countingDown: false,
+
+    };
+  }
+
+  // Using property initializer syntax to avoid need to bind, since arrow functions don't create their own this context and use value of enclosing context instead. transform-class-properties Babel plugin necessary to use this syntax (included in Create React App). Refer to https://itnext.io/property-initializers-what-why-and-how-to-use-it-5615210474a3 for more details
+
+  // DURATION CHANGES
+
+  decreaseBreakDuration = () => {
+
+  }
+
+  increaseBreakDuration = () => {
+
+  }
+
+  decreaseSessionDuration = () => {
+
+  }
+
+  increaseSessionDuration = () => {
+
+  }
+
+  // PLAY, PAUSE, RESTART BUTTONS
+
+  handlePlayBtnClick = () => {
+
+  }
+
+  handlePauseBtnClick = () => {
+
+  }
+
+  handleRestartBtnClick = () => {
+
+  }
+
+
+  render() {
+    return (
+      <Timer
+        decreaseBreakDuration={this.props.decreaseBreakDuration}
+        increaseBreakDuration={this.props.increaseBreakDuration}
+        decreaseSessionDuration={this.props.decreaseSessionDuration}
+        increaseSessionDuration={this.props.increaseSessionDuration}
+
+        handlePlayBtnClick={this.handlePlayBtnClick}
+        handlePauseBtnClick={this.handlePauseBtnClick}
+        handleRestartBtnClick={this.handleRestartBtnClick}
+      />
+    );
+  };
 }
 
 export default App;
---
+++ src/Timer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Timer = (props) => (
+
+<>
+
+  {/* BREAK LENGTH CONTROL */}
+  <div></div>
+
+  {/* SESSION LENGTH CONTROL */}
+  <div></div>
+
+  {/* TIME REMAINING */}
+  <div></div>
+
+  {/* PLAY, PAUSE, RESTART BUTTONS */}
+
+
+</>
+
+);
+
+export default Timer;
