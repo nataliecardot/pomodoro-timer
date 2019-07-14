d23025d4ab7f9be7c97191af11147c16c1d07181
Natalie-Cardot <nataliecardot@gmail.com>
2019-07-13 21:21:47 -0700

    Updates

---
+++ precache-manifest.13766b6d045941ccf35ae69c66d618da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e605f8f7f65282981b8bc3af94b8b620",
+    "url": "/pomodoro-timer/index.html"
+  },
+  {
+    "revision": "b71d3fdb843e00455a7c",
+    "url": "/pomodoro-timer/static/css/main.e3f773cc.chunk.css"
+  },
+  {
+    "revision": "7fa7fface8bf080a1c69",
+    "url": "/pomodoro-timer/static/js/2.9fa495b5.chunk.js"
+  },
+  {
+    "revision": "b71d3fdb843e00455a7c",
+    "url": "/pomodoro-timer/static/js/main.e8605165.chunk.js"
+  },
+  {
+    "revision": "8d84c672a99e0f0ece83",
+    "url": "/pomodoro-timer/static/js/runtime~main.2a0df6ba.js"
+  }
+]);
